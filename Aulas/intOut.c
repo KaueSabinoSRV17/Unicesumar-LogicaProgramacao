86e40af0bc5a2218aaa65b12a1ea0b50903357ef
Kauê Sabino VIllaça de Almeida <kauevillaca2018@gmail.com>
2022-06-21 22:25:52 -0300

    .

---
+++ Aulas/intOut.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    printf("Digite o valor a ser printado: ");
+    int num;
+    scanf("%d", &num);
+    printf("\n");
+    printf("%d", num);
+    
+    return 0;
+}
