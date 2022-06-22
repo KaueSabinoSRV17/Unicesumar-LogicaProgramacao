58f839962d288e8c31891be4870d4326841e58c6
Kauê Sabino VIllaça de Almeida <kauevillaca2018@gmail.com>
2022-06-21 23:22:38 -0300

    Vetor em C

---
+++ Aulas/vetores.c
@@ -0,0 +1,14 @@
+int main(int argc, char const *argv[])
+{
+
+    int i;
+    int nms[10] = {1, 2, 3, 4, 5};
+
+    for (i = 0; i <= 4; i++)
+    {
+        printf("%d ", nms[i]);
+    }
+    
+
+    return 0;
+}
